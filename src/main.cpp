d01cda265ece00aeec8cf650e4836b0d9192b0e3
HeHuMing <hehuming434@gmail.com>
2025-07-27 07:54:12 +0000

    Add main.cpp with JSON parsing demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+#include "../include/json.hpp"
+#include <fmt/core.h>
+using json=nlohmann::json;
+int main(){
+    std::string json_str=R"(
+        {
+             "name":"Alice",
+             "age":25,
+             "skills":["C++","python"]
+         }
+    )";
+    auto data=json::parse(json_str);
+    std::string new_json_str=fmt::format(
+        R"({{"name":"{}","skills":{},"gender":"Female"}})",
+        data["name"].get<std::string>(),
+        data["skills"].dump()
+     );
+     auto new_data=json::parse(new_json_str);
+     std::cout<<"Original JSON:\n"<<data.dump(2)<<"\n\n";
+     std::cout<<"Formatted JSON:\n"<<new_data.dump(2)<<std::endl;
+     return 0;
+}
